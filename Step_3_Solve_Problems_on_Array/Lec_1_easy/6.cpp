ae78d0c8690f7049cdbc95ed6e0b8c65547b85bf
Bhupesh-Khordia <143842550+Bhupesh-Khordia@users.noreply.github.com>
2025-04-09 02:04:46 +0530

    Update 6.cpp

--- Step_3_Solve_Problems_on_Array/Lec_1_easy/6.cpp
+++ Step_3_Solve_Problems_on_Array/Lec_1_easy/6.cpp
@@ -1,15 +1,10 @@
+
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-        int j = 0;
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] != 0) {
-                nums[j] = nums[i];
-                j++;
-            }
-        }
-        for(int i = j; i < nums.size(); i++) {
-            nums[i] = 0;
-        }
+    void rotate(vector<int>& nums, int k) {
+        k %= nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };

ec8a1cd4bb16103e7d2ce2c2e518f69e8ac604d0
Bhupesh-Khordia <143842550+Bhupesh-Khordia@users.noreply.github.com>
2025-04-09 02:05:02 +0530

    Update 7.cpp

--- Step_3_Solve_Problems_on_Array/Lec_1_easy/7.cpp
+++ Step_3_Solve_Problems_on_Array/Lec_1_easy/7.cpp
@@ -1,25 +1,16 @@
 
 class Solution {
-  public:
-    // Function to find element in sorted array
-    // arr: input array
-    // k: element to be searched
-    
-    bool binarySearch(vector<int>& arr, int k, int l, int r) {
-        if (l > r) return false;
-
-        int mid = l + (r - l) / 2;
-
-        if (arr[mid] == k) return true;
-        else if (arr[mid] > k)
-            return binarySearch(arr, k, l, mid - 1);
-        else
-            return binarySearch(arr, k, mid + 1, r);
-    }
-    
-    bool searchInSorted(vector<int>& arr, int k) {
-        
-        return binarySearch(arr, k, 0, arr.size() - 1);
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] != 0) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        for(int i = j; i < nums.size(); i++) {
+            nums[i] = 0;
+        }
     }
 };
-

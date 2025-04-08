f35f883dbca176a98f0067df83e341f328a4d4ef
Bhupesh-Khordia <143842550+Bhupesh-Khordia@users.noreply.github.com>
2025-04-08 23:48:24 +0530

    Create 3.cpp

---
+++ Step_3_Solve_Problems_on_Array/Lec_1_easy/3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool sorted(vector<int>& arr){
+        for(int i = 0; i < arr.size() - 1; i++){
+            if(arr[i+1] < arr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool check(vector<int>& nums) {
+        vector<int> temp(nums);
+        for(int x = 0; x < nums.size(); x++) {
+            for(int i = 0; i < nums.size(); i++){
+                temp[(i + x) % (nums.size())] = nums[i];
+            }
+            if(sorted(temp)){
+                return true;
+            }
+        }
+        return false;
+    }
+};

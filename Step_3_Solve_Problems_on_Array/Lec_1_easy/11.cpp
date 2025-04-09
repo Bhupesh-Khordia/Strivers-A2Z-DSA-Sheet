91a49dbe38ac3a66e7e2f857385f061de3b550fd
Bhupesh-Khordia <143842550+Bhupesh-Khordia@users.noreply.github.com>
2025-04-09 23:37:44 +0530

    Create 11.cpp

---
+++ Step_3_Solve_Problems_on_Array/Lec_1_easy/11.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int ans = 0;
+        int cnt = 0;
+        if(nums.size() == 1) return nums[0] == 1;
+        if(nums[0] == 1){
+            cnt = 1;
+            ans = max(ans, cnt);
+        }
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[i] == 1) {
+                cnt++;
+                ans = max(ans, cnt);
+            } else {
+                cnt = 0;
+            }
+        }
+        return ans;
+    }
+};

f23bca4ff240d244682eb21d5f22f44fb75c3e55
Bhupesh-Khordia <143842550+Bhupesh-Khordia@users.noreply.github.com>
2025-04-09 23:23:19 +0530

    Create 9.cpp

---
+++ Step_3_Solve_Problems_on_Array/Lec_1_easy/9.cpp
@@ -0,0 +1,60 @@
+class Solution {
+  public:
+    // a,b : the arrays
+    // Function to return a list containing the union of the two arrays.
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        int i = 0, j = 0;
+        vector<int> ans;
+        while(i < a.size() && j < b.size()) {
+            if(a[i] < b[j]) {
+                if(ans.empty()) {
+                    ans.push_back(a[i]);
+                    i++;
+                } else {
+                    if(ans[ans.size() - 1] == a[i]) i++;
+                    else {
+                        ans.push_back(a[i]);
+                        i++;
+                    }
+                }
+            } else {
+                if(ans.empty()) {
+                    ans.push_back(b[j]);
+                    j++;
+                } else {
+                    if(ans[ans.size() - 1] == b[j]) j++;
+                    else {
+                        ans.push_back(b[j]);
+                        j++;
+                    }
+                }
+            }
+        }
+        while(i < a.size() ) {
+            if(ans.empty()) {
+                ans.push_back(a[i]);
+                i++;
+            } else {
+                if(ans[ans.size() - 1] == a[i]) i++;
+                else {
+                    ans.push_back(a[i]);
+                    i++;
+                }
+            }
+        }
+        while(j < b.size()) {
+            if(ans.empty()) {
+                ans.push_back(b[j]);
+                j++;
+            } else {
+                if(ans[ans.size() - 1] == b[j]) j++;
+                else {
+                    ans.push_back(b[j]);
+                    j++;
+                }
+            }
+        }
+        return ans;
+        
+    }
+};

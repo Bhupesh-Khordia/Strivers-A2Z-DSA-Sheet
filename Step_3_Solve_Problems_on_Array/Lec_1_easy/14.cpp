70129216743dddfdd8fa52b1030f4f054ea5c780
Bhupesh-Khordia <143842550+Bhupesh-Khordia@users.noreply.github.com>
2025-04-10 23:30:57 +0530

    Create 14.cpp

---
+++ Step_3_Solve_Problems_on_Array/Lec_1_easy/14.cpp
@@ -0,0 +1,47 @@
+class Solution {
+  public:
+    int longestSubarray(vector<int>& arr, int k) {
+        // We can use two pointer approach (both starting from same end of array) if only positive elements in array.
+        /*
+        int i = 0, j = 0;
+        int sum = arr[0], ans = 0;
+        while(i < arr.size() && j < arr.size()) {
+            if(sum == k) {
+                ans = max(ans, j - i + 1);
+                j++;
+                if(j != arr.size()) sum += arr[j];
+            } else if (sum < k) {
+                j++;
+                if(j != arr.size()) sum += arr[j];
+            } else {
+                i++;
+                sum -= arr[i - 1];
+            }
+        }
+        return ans;
+        */
+        
+        
+        // When positives & negatives both in array.
+        // /*
+        unordered_map<int, int> map;
+        int sum = 0;
+        int ans = 0;
+        for(int i = 0; i < arr.size(); i++) {
+            sum += arr[i];
+            if(sum == k) {
+                ans = max(ans, i + 1);
+            }
+            int remaining = sum - k;
+            if(map.find(remaining) != map.end()) {
+                int length = map[remaining];
+                ans = max(ans, i + 1 - length);
+            }
+            if(map.find(sum) == map.end()) {
+                map[sum] = i + 1;
+            }
+        }
+        return ans;
+        // */
+    }
+};

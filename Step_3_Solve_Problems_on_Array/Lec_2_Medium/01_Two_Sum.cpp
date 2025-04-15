b07e3f221422241c28e34f62ba532f075d412727
Bhupesh-Khordia <143842550+Bhupesh-Khordia@users.noreply.github.com>
2025-04-15 14:37:36 +0530

    Update 01_Two_Sum.cpp

--- Step_3_Solve_Problems_on_Array/Lec_2_Medium/01_Two_Sum.cpp
+++ Step_3_Solve_Problems_on_Array/Lec_2_Medium/01_Two_Sum.cpp
@@ -2,6 +2,8 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // Another way is to sort and use two pointer like binary search
+        
         unordered_map<int, int> map;
         for(int i = 0; i < nums.size(); i++) {
             if(map.find(target - nums[i]) == map.end()) {

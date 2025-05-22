f4e361ccd9021a96f96c7de5dd0dc877e5b89313
Bhupesh-Khordia <bhupeshy510@gmail.com>
2025-05-22 23:07:52 +0530

    Add implementation for Merge Intervals problem

---
+++ Step_12_Greedy_Algorithms_[Easy,_Medium_or_Hard]/Lec_02_Medium_or_Hard/10_Merge_Intervals.cpp
@@ -0,0 +1,55 @@
+// https://leetcode.com/problems/merge-intervals/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+private:
+    static bool cmp (vector<int> a, vector<int> b) {
+        return a[0] < b[0];
+    }
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        // Extra Space - O(n)
+        /*
+        sort(intervals.begin(), intervals.end(), cmp);
+
+        int start = intervals[0][0];
+        int end = intervals[0][1];
+        vector<vector<int>> ans;
+        int n = intervals.size();
+
+        for(int i = 1; i < n; i++) {
+            if(intervals[i][0] > end) {
+                // No overlap
+                ans.push_back({start, end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            } else {
+                // Overlap
+                end = max(end, intervals[i][1]);
+            }
+        }
+        ans.push_back({start, end});
+        return ans;
+        */
+
+
+        // No extra space
+        sort(intervals.begin(), intervals.end());
+        int k = 0; // Index for
+        
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[k][1] >= intervals[i][0]) { // Overlap detected
+                intervals[k][1] = max(intervals[k][1], intervals[i][1]); 
+            } else {
+                k++;
+                intervals[k] = intervals[i]; // Replace in-place
+            }
+        }
+        
+        intervals.resize(k + 1); 
+        return intervals;
+    }
+};

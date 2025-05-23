8a18076b896ae617a474faa0ae94d5bf897f7da3
Bhupesh-Khordia <bhupeshy510@gmail.com>
2025-05-23 17:28:21 +0530

    Add implementation for printing all prime factors of a number

---
+++ Step_08_Bit_Manipulation_[Concepts_&_Problems]/Lec_03_Advanced_Maths/01_Print_Prime_Factors_of_a_Number.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// https://www.geeksforgeeks.org/problems/prime-factors5052/1
+
+class Solution {
+  public:
+    vector<int> AllPrimeFactors(int N) {
+        vector<int> ans;
+        for(int i = 2; i <= sqrt(N); i++) {
+            if(N % i == 0) {
+                ans.push_back(i);
+                while(N % i == 0) N = N / i;
+            }
+        }
+        if(N != 1) ans.push_back(N);
+        return ans;
+    }
+};

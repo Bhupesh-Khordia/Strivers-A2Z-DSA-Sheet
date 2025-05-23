17ea59a1f06f1840b43daeba7ada19511460e873
Bhupesh-Khordia <bhupeshy510@gmail.com>
2025-05-23 18:18:14 +0530

    Add implementation of power function (x^n) using iterative method

---
+++ Step_08_Bit_Manipulation_[Concepts_&_Problems]/Lec_03_Advanced_Maths/05_Power(n,x).cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/powx-n/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;  // Use long long to handle INT_MIN safely
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double result = 1.0;
+        while (N > 0) {
+            if (N % 2 == 1) {
+                result *= x;
+                N = N - 1;
+            } else {
+                // x ^ y = (x ^ 2) ^ (y / 2)
+                x *= x;
+                N /= 2;
+            }
+        }
+        return result;
+    }
+};

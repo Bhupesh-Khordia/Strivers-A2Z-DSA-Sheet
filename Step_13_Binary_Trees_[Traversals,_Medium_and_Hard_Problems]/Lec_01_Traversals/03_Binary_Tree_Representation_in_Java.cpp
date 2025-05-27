86a37f9ebe957c22284e6ade0484cffe94243aff
Bhupesh-Khordia <bhupeshy510@gmail.com>
2025-05-27 13:28:00 +0530

    Add implementation for Binary Tree Representation in Java

---
+++ Step_13_Binary_Trees_[Traversals,_Medium_and_Hard_Problems]/Lec_01_Traversals/03_Binary_Tree_Representation_in_Java.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// https://www.geeksforgeeks.org/problems/binary-tree-representation/1
+
+// I dont know JAVA :(
+
+/*struct node {
+  int data;
+  struct node *left;
+  struct node *right;
+};
+
+struct node *newNode(int data) {
+  struct node *node = (struct node *)malloc(sizeof(struct node));
+
+  node->data = data;
+
+  node->left = NULL;
+  node->right = NULL;
+  return (node);
+}*/
+
+class Solution{
+public:
+    void create_tree(node* root0, vector<int> &v){
+        deque<node*> que;
+        que.push_back(root0);
+        
+        int i=0;
+        while(!que.empty()) {
+            node* cur = que.front();
+            que.pop_front();
+            
+            if((2*i + 1) < 7) {
+                cur->left = newNode(v[2*i + 1]);
+                que.push_back(cur->left);
+            }
+            if((2*i + 2) < 7) {
+                cur->right = newNode(v[2*i + 2]);
+                que.push_back(cur->right);
+            }
+            i++;
+        }
+    }
+
+};

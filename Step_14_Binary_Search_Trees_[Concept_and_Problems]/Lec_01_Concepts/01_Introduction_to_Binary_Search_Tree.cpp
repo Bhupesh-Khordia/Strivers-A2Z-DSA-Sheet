ebc1755f3da14e9b53f484bca4ebc784e995cdf9
Bhupesh-Khordia <bhupeshy510@gmail.com>
2025-05-30 12:03:32 +0530

    Add initial implementation of binary search tree with isBSTTraversal method

---
+++ Step_14_Binary_Search_Trees_[Concept_and_Problems]/Lec_01_Concepts/01_Introduction_to_Binary_Search_Tree.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// https://www.geeksforgeeks.org/problems/binary-search-trees/1
+
+
+class Solution {
+  public:
+    bool isBSTTraversal(vector<int>& arr) {
+        for(int i = 1; i < arr.size(); i++) {
+            if(arr[i] <= arr[i - 1]) return false;
+        }
+        return true;
+    }
+};

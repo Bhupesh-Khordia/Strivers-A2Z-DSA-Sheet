78e5b6fc3c254a2b9d43ea73f85a61ed8372a989
Bhupesh-Khordia <bhupeshy510@gmail.com>
2025-06-06 17:30:37 +0530

    Add implementation of Minimum Coins problem using tabulation dynamic programming

---
+++ Step_16_Dynamic_Programming_[Patterns_and_Problems]/Lec_04_DP_on_Subsequences/07_Minimum_Coins_(DP-20).cpp
@@ -0,0 +1,86 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+// https://leetcode.com/problems/coin-change/description/
+
+class Solution {
+private:
+
+    int solver (vector<int> &num, int x) {
+        
+        if(x == 0) return 0;
+
+        int ans = INT_MAX;
+
+        if(x < 0) return ans;
+
+        for(auto i : num) {
+            int mini = min(ans, solver(num, x-i));
+            if(mini != INT_MAX)
+                ans = min(1 + mini, ans);
+        }
+
+        return ans;
+    }
+
+    int solverDP (vector<int> &num, int x, vector<int> &dp) {
+        
+        if(x == 0) return 0;
+
+        if(dp[x] != -1) {
+            return dp[x];
+        }
+
+        int ans = INT_MAX;
+
+        if(x < 0) return ans;
+
+        for(auto i : num) {
+            int mini = min(ans, solver(num, x-i));
+            if(mini != INT_MAX)
+                ans = min(1 + mini, ans);
+        }
+
+        return dp[x] = ans;
+    }
+
+
+    // Tabulation Approach
+
+    int solverTab (vector<int>& num, int x) {
+        vector<int> dp(x + 1, INT_MAX);
+        
+        // Base Case
+        dp[0] = 0;
+
+        for(int i = 0; i <= x; i++) {
+            for(auto j : num) {
+                if(i-j >=0 && dp[i - j] != INT_MAX)
+                    dp[i] = min(dp[i], 1 + dp[i-j]);
+            }
+        }
+
+        return dp[x];
+    }
+
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        // Recursive Approah.
+
+        // int ans = solver(coins, amount);
+        // return ans == INT_MAX ? -1 : ans;
+
+
+        // DP Approach.
+
+        // vector<int> dp (amount + 1, -1);
+        // int ans = solverDP(coins, amount, dp);
+        // return ans == INT_MAX ? -1 : ans;
+
+
+        // Tabulation Approach.
+
+        int ans = solverTab(coins, amount);
+        return ans == INT_MAX ? -1 : ans;
+    }
+};

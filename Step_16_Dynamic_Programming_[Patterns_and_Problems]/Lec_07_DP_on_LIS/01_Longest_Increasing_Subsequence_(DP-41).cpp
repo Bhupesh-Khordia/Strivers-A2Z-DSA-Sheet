8e96860a129d552391d81ef5778d5ef823c5c4dd
Bhupesh-Khordia <bhupeshy510@gmail.com>
2025-06-08 11:08:01 +0530

    Add implementation of Longest Increasing Subsequence problem using dynamic programming

---
+++ Step_16_Dynamic_Programming_[Patterns_and_Problems]/Lec_07_DP_on_LIS/01_Longest_Increasing_Subsequence_(DP-41).cpp
@@ -0,0 +1,140 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+// https://leetcode.com/problems/longest-increasing-subsequence/description/
+
+class Solution {
+private:
+    // Recursion Solulu
+    int  solveRec(vector<int> &arr, int n, int currIndex, int prevIndex) {
+        if(currIndex == n)  return  0;
+        
+        // Include
+        int include = 0;
+        if(prevIndex == -1 || arr[currIndex] > arr[prevIndex])  {
+            include = 1 + solveRec (arr, n, currIndex + 1, currIndex);
+        }
+        
+        // Exclude 
+        int exclude = 0 + solveRec (arr, n, currIndex + 1, prevIndex);
+        
+        return max(include, exclude);
+    }
+    
+    // DP Solulu
+    int  solveDP(vector<int> &arr, int n, int currIndex, int prevIndex, vector<vector<int>> &dp) {
+        if(currIndex == n)  return  0;
+        
+        // prevIndex + 1 is done for the case when prevIndex = -1
+        if(dp[currIndex][prevIndex + 1]  != -1)  return dp[currIndex][prevIndex + 1];
+        
+        // Include
+        int include = 0;
+        if(prevIndex == -1 || arr[currIndex] > arr[prevIndex])  {
+            include = 1 + solveDP (arr, n, currIndex + 1, currIndex, dp);
+        }
+        
+        // Exclude 
+        int exclude = 0 + solveDP (arr, n, currIndex + 1, prevIndex, dp);
+        
+        return dp[currIndex][prevIndex + 1] = max(include, exclude);
+    }
+    
+    // Tabulation Solulu
+    int solveTab(vector<int> &arr, int n) {
+        vector<vector<int>> dp(n + 1, vector<int> (n + 1, 0));
+        
+        for(int currIndex = n - 1; currIndex >= 0; currIndex--) {
+            for(int prevIndex = currIndex - 1; prevIndex >= -1; prevIndex--) {
+                // Include
+                int include = 0;
+                if(prevIndex == -1 || arr[currIndex] > arr[prevIndex])  {
+                    include = 1 + dp[currIndex + 1][currIndex + 1]; // Prev Index is used by increment of 1 so, currIndex + 1 will be used as prev.
+                }
+                
+                // Exclude 
+                int exclude = 0 + dp[currIndex + 1][prevIndex + 1];  // prevIndex + 1 to ensure case of -1
+                
+                dp[currIndex][prevIndex + 1] = max(include, exclude);
+            }
+        }
+        
+        return dp[0][0];
+    }
+    
+    
+    // Space Optimized Solulu
+    int solveSO(vector<int> &arr, int n) {
+        // vector<vector<int>> dp(n + 1, vector<int> (n + 1, 0));
+        vector<int> next(n + 1, 0);
+        vector<int> curr(n + 1, 0);
+        
+        for(int currIndex = n - 1; currIndex >= 0; currIndex--) {
+            for(int prevIndex = currIndex - 1; prevIndex >= -1; prevIndex--) {
+                // Include
+                int include = 0;
+                if(prevIndex == -1 || arr[currIndex] > arr[prevIndex])  {
+                    include = 1 + next[currIndex + 1]; // Prev Index is used by increment of 1 so, currIndex + 1 will be used as prev.
+                }
+                
+                // Exclude 
+                int exclude = 0 + next[prevIndex + 1];  // prevIndex + 1 to ensure case of -1
+                
+                curr[prevIndex + 1] = max(include, exclude);
+            }
+            next = curr;
+        }
+        
+        return next[0];
+    }
+    
+    
+    // DP With Binary Search Solulu (Best) (TC - O(N * log N), SC -O(N))
+    int solveDPWithBS(vector<int> &arr, int n) {
+        vector<int> ans;
+        ans.push_back(arr[0]);
+        
+        for(int i = 1; i < n; i++ ) {
+            if(arr[i] > ans.back()) {
+                ans.push_back(arr[i]);
+            } else {
+                int index = lower_bound(ans.begin(), ans.end(), arr[i]) - ans.begin();
+                ans[index] = arr[i];
+            }
+        }
+        
+        return ans.size();
+    }
+
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        
+        // Recursive Solulu
+        
+        // return solveRec(nums, n, 0,  -1);
+        
+        
+        // DP Solulu
+        
+        // vector<vector<int>> dp(n, vector<int> (n + 1, -1));
+        // return solveDP(nums, n, 0, -1, dp);
+        
+        
+        // Tabulation Solulu
+        
+        // return solveTab(nums, n);
+        
+        
+        
+        // Space Optimized Solulu
+        
+        // return solveSO(nums,  n);
+        
+        
+        
+        // DP With Binary Search Solulu (Best) (TC - O(N * log N), SC -O(N))
+        
+        return solveDPWithBS (nums, n);
+    }
+};

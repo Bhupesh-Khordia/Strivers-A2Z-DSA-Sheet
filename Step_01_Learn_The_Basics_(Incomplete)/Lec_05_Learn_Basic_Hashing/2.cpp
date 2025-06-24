dd42ac0f0d508f8b3e32f06709937e99b7c128bb
Bhupesh-Khordia <bhupeshy510@gmail.com>
2025-06-24 15:50:55 +0530

    Add missing includes and comments for frequency count function

--- Step_01_Learn_The_Basics_(Incomplete)/Lec_05_Learn_Basic_Hashing/2.cpp
+++ Step_01_Learn_The_Basics_(Incomplete)/Lec_05_Learn_Basic_Hashing/2.cpp
@@ -1,3 +1,8 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+// https://www.geeksforgeeks.org/problems/frequency-of-array-elements-1587115620/1
+
 class Solution {
   public:
     // Function to count the frequency of all elements from 1 to N in the array.
